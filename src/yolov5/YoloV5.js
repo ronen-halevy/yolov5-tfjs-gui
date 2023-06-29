dbad758fc57b337cc34b38a81f0e966b589c1da8
ronen-halevy <ronen567@gmail.com>
2023-06-29 12:22:39 +0300

    clean dup colors

--- src/yolov5/YoloV5.js
+++ src/yolov5/YoloV5.js
@@ -1,44 +1,3 @@
-// crop masks pixels which are not inside a bbox.
-class Colors {
-	constructor(
-		canvas,
-		lineWidth,
-		lineColor,
-		font,
-		textColor,
-		textBackgoundColor
-	) {
-		self.palette = [
-			[0xff, 0x38, 0x38],
-			[0xff, 0x9d, 0x97],
-			[0xff, 0x70, 0x1f],
-			[0xff, 0xb2, 0x1d],
-			[0xcf, 0xd2, 0x31],
-			[0x48, 0xf9, 0x0a],
-			[0x92, 0xcc, 0x17],
-			[0x3d, 0xdb, 0x86],
-			[0x1a, 0x93, 0x34],
-			[0x00, 0xd4, 0xbb],
-			[0x2c, 0x99, 0xa8],
-			[0x00, 0xc2, 0xff],
-			[0x34, 0x45, 0x93],
-			[0x64, 0x73, 0xff],
-			[0x00, 0x18, 0xec],
-			[0x84, 0x38, 0xff],
-			[0x52, 0x00, 0x85],
-			[0xcb, 0x38, 0xff],
-			[0xff, 0x95, 0xc8],
-			[0xff, 0x37, 0xc7],
-		];
-		self.n = self.palette.length;
-	}
-	// self.pp = 'r'
-	getColor(i) {
-		const c = self.palette[i % self.n];
-		return [c[0] / 255, c[1] / 255, c[2] / 255];
-	}
-}
-
 const masks = (maskPatterns, colors, preprocImage, alpha) => {
 	var colors = tf.cast(colors, 'float32').reshape([-1, 1, 1, 3]); //shape(n,1,1,3)
 	maskPatterns = tf.cast(maskPatterns, 'float32'); // (n,h,w,1)
@@ -68,7 +27,7 @@ class YoloV5 {
 		this.scoreTHR = scoreTHR;
 		this.iouTHR = iouTHR;
 		this.maxBoxes = maxBoxes;
-		this.colors = new Colors();
+		// this.colors = new Colors();
 		self.palette = [
 			[0xff, 0x38, 0x38],
 			[0xff, 0x9d, 0x97],
